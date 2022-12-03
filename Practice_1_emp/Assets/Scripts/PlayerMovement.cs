f8e564785f47a56e2c156cf27be6e029540d909d
BajagainSatish <106542315+BajagainSatish@users.noreply.github.com>
2022-12-03 20:28:04 +0545

    Movement replaced by bot

--- Practice_1_emp/Assets/Scripts/PlayerMovement.cs
+++ Practice_1_emp/Assets/Scripts/PlayerMovement.cs
@@ -9,8 +9,6 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D playerRb;
     private GameManager gameManager;
     public float speed = 10;
-    float horiInput;
-
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +19,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        playerRb.AddForce(Vector2.right * speed, ForceMode2D.Force);
-
-            //playerRb.velocity = new Vector2(horizontalInput * (speed / 2), playerRb.velocity.y);
-
+        //playerRb.velocity = new Vector2(Input.GetAxis("Horizontal") * (speed / 2), playerRb.velocity.y);
         if (Input.GetKey(KeyCode.Space)) {
             playerRb.velocity = new Vector2(playerRb.velocity.x, speed);
             Debug.Log("Jump");
         }
-        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,5 +31,18 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Enemy")) {
             Destroy(collision.gameObject);
         }
+    }
+
+    public void onClickLeft()
+    {
+        playerRb.AddForce(Vector2.left * speed, ForceMode2D.Force);
+    }
+    public void onClickRight()
+    {
+        playerRb.AddForce(Vector2.right * speed, ForceMode2D.Force);
+    }
+    public void OnClickJump()
+    {
+        playerRb.AddForce(Vector2.up * speed, ForceMode2D.Force);
     }
 }
